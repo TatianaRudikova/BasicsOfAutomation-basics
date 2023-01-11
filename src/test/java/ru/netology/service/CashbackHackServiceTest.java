6a70877a44922bc0664b6b1a596dd550ff7bcfb1
Tatiana Rudikova <tatjana_tv@list.ru>
2023-01-11 21:50:49 +0300

    добавлены тесты

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,16 +2,32 @@ package ru.netology.service;
 
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.AssertJUnit.*;
 
 public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void testRemain() {
-        int amount = 1500;
-        int expected = 500;
-        CashbackHackService service = new CashbackHackService();
-        int actual = service.remain(amount);
+    public void shouldAmountEquallyReturned() {
+
+        int expected = 1000;
+        int actual = service.remain(1000);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountLessReturned() {
+
+        int expected = 1000;
+        int actual = service.remain(0);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldAmountMoreReturned() {
+
+        int expected = 0;
+        int actual = service.remain(1000);
         assertEquals(actual, expected);
     }
 }
